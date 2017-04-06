549fb37e3127455f531111b625596a76a85a84d6
dlofstrom <lofstrom.daniel@gmail.com>
2017-04-06 16:50:02 +0200

    Solution to problem 2

---
+++ problems/problem2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "problem.h"
+#include "common.h"
+
+void problem(char *info)
+{
+    uint32_t fib[2] = {1,2};
+    printf("Fibonacci numbers:\n%u, %u", fib[0], fib[1]);
+    uint32_t sum = 2; //Starting with 1,2
+
+    //Loop until next fibonacci number is > 4000000
+    while (fib[0] + fib[1] < 4000000) {
+        //Generate new fibonacci number
+        fib[1] += fib[0];
+        fib[0] = fib[1] - fib[0];
+        
+        //Add to sum if even
+        if (fib[1] % 2 == 0) sum += fib[1];
+        printf(", %u", fib[1]);
+    }
+    
+    printf("\n\nAnswer: %u\n", sum);
+}

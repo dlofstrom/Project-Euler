b2036b51f2fcd6998709cdd0e789a532e3eb5b41
dlofstrom <lofstrom.daniel@gmail.com>
2017-04-30 15:49:23 +0200

    Solution to problem 9

---
+++ problems/problem9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "problem.h"
+#include "common.h"
+
+void problem(char *info)
+{
+    uint32_t abc = 0;
+    //Generate possible numbers
+    uint32_t a,b,c;
+    uint32_t a2,b2,c2;
+    for (a = 1; a < 1000; a++) {
+        a2 = pow(a, 2);
+        //b can only be 1 <= b < 1000-a
+        for (b = 1; b < 1000-a; b++) {
+            b2 = pow(b, 2);
+
+            //c can only be 1000-a-b
+            c = 1000 - a - b;
+            c2 = pow(c, 2);
+            
+            //Check if Pythagorean triplet
+            if (a2 + b2 == c2) {
+                abc = a*b*c;
+            }
+        }
+    }
+    
+    printf("Answer: %u\n", abc);
+}

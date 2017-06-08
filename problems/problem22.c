88b9d23d6879358ebba5cc0881c7a70b5c449f1e
dlofstrom <lofstrom.daniel@gmail.com>
2017-06-08 09:30:35 +0200

    Solution to problem 22, insertion sort

--- problems/problem22.c
+++ problems/problem22.c
@@ -9,9 +9,14 @@
 //Node for linked list
 typedef struct nn {
     char name[20];
-    uint32_t value;
     struct nn *next;
 } name_node_t;
+
+uint32_t name_value(char *name) {
+    uint32_t v = 0;
+    while (*name != '\0') v += *(name++) - 'A' + 1;
+    return v;
+}
 
 void problem(char *info)
 {
@@ -21,66 +26,54 @@ void problem(char *info)
     char *names = get_webpage("https://projecteuler.net/project/resources/p022_names.txt");
 
     //Load names into list
-    name_node_t *list = (name_node_t*)malloc(sizeof(name_node_t));
-    list->value = 0; //Last node is "empty"
-    name_node_t *last = list;
+    name_node_t *list = NULL;
+    name_node_t *np;
     char *start = names;
     char *stop;
-    uint32_t v;
     while (*start != '\0') {
         //Move start to first letter in name
         while (*start == '\"' || *start == ',') start++;
         stop = start;
-        v = 0;
-        //Move stop to end of name and calculate value
-        while (*stop != '\"') {
-            v += *stop - 'A' + 1;
-            stop++;
-        }
+        //Move stop to end of name
+        while (*stop != '\"') stop++;
 
         //Create list node
         name_node_t *node = (name_node_t*)malloc(sizeof(name_node_t));
         strncpy(node->name, start, stop-start);
         node->name[stop-start] = '\0';
-        node->value = v;
-        node->next = list;
-        list = node;
-
+        
+        //Insertion sort directly in list
+        np = list;
+        if (list == NULL || strcmp(node->name, list->name) < 0) {
+            node->next = list;
+            list = node;
+        } else {
+            while (np->next != NULL) {
+                if (strcmp(node->name, np->next->name) < 0) {
+                    node->next = np->next;
+                    np->next = node;
+                    break;
+                }
+                np = np->next;
+            }
+            if (np->next == NULL) {
+                node->next = np->next;
+                np->next = node;
+            }
+        }
         //Move start
         start = stop + 1;
     }
     free(names);
 
-    //Sort names (Bubble sort)
-    name_node_t *np, *prev;
-    v = 1;
-    while (list->value != 0) {
-        np = list;
-        prev = last;
-        //printf("%u", v);
-        while (np->next->value != 0) {
-            //Compare names
-            if (strcmp(np->name, np->next->name) < 0) {
-                //printf("Switch %s with %s\n", npt->name, npt->next->name);
-                //Switch so that "smallest" string is at the bottom
-                name_node_t *t = np->next;
-                np->next = t->next;
-                if (prev->value != 0) prev->next = t;
-                t->next = np;
-                if (np == list) list = t;
-                prev = t;
-            } else {
-                prev = np;
-                np = np->next;
-
-            }
-        }
-        //Print add and remove last (alphabetically first) node
-        total += v * (np->value);
-        if (np == list) list = np->next;
-        prev->next = np->next;
-        free(np);
+    //Calculate total
+    np = list;
+    uint32_t v = 1;
+    while (np != NULL) {
+        total += v * name_value(np->name);
+        np = np->next;
         v++;
     }
+    
     printf("Answer: %llu\n", total);
 }
